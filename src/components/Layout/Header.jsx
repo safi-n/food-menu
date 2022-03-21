239d9296ed929e846f36e17c54b26fa0a43749b8
Safi <Nurzai.safi@gmail.com>
2022-03-21 13:53:25 +0300

    Nav bar positioning issue solved

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -6,14 +6,16 @@ const Header = () => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h2>React Menu</h2>
+        <div>
+          <h2>React Menu</h2>
+          <button> Cart </button>
+        </div>
       </header>
 
       <div className={classes['img-container']}>
         <img
         src={mealHeader}
-        alt="header picture"
-        className={classes.headerImg} />
+        alt="header picture"/>
       </div>
     </Fragment>
   )
