b6e5d73211912b6bfefe84c906d712f749f69d3b
Safi <Nurzai.safi@gmail.com>
2022-03-23 21:55:54 +0300

    Cart style done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,18 +5,18 @@ import classes from './cart.module.css'
 const Cart = () => {
   const cartItems = [
     { id: "m1", name: "shorwa", description: "yumm yuma", price: "12" },
-  ].map((item) => <li>{item.name}</li>);
+  ].map((item) => <li className={classes['list-item']}>{item.name}</li>);
 
   return (
   <Modal>
     {cartItems}
-    <div>
-      <span>Total amount</span>
+    <div className={classes.amount}>
+      <span className={classes['amount-total']}>Total amount</span>
       <span>21</span>
     </div>
-    <div>
-      <button>close</button>
-      <button>order</button>
+    <div className={classes['button-area']}>
+      <button className={classes['btn-danger']}>Close</button>
+      <button className={classes['btn-success']}>Order</button>
     </div>
   </Modal>
   );
