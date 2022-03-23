50cd4fa7d5aae20d791f39ecaee47bb8ba66b731
Safi <Nurzai.safi@gmail.com>
2022-03-23 22:38:39 +0300

    useState to cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import './App.css';
 import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Modal from './components/UI/Modal';
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false)
+  const showCartHandler = () => {
+    setCartIsShown(true)
+  }
+  const hideCartHandler = () => {
+    setCartIsShown(false)
+  }
   return (
     <Fragment>
-      <Cart />
+      {cartIsShown && <Cart />}
       <Header />
       <main>
         <Meals />
       </main>
       <footer className="footer">
-        <h2>© 2022</h2>
+        <h2>&copy; 2022</h2>
       </footer>
     </Fragment>
   );
