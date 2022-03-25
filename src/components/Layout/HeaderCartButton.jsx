606482b66449156db53d4660c0daa07fc80fcaa4
Safi <Nurzai.safi@gmail.com>
2022-03-25 23:28:55 +0300

    FEAT: btn bump animation added

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,14 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import classes from "./headerCartBtn.module.css";
 import CartContext from "../../context/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnBumped, setBtnBumped] = useState(false)
   const cartCtx = useContext(CartContext);
-  const buttonClasses = `${classes['cart-button']} ${classes.bump}`
-  const numberOfCartItem = cartCtx.items.reduce((curNumber, item) => {
+  const {items} = cartCtx;
+  const buttonClasses = `${classes['cart-button']} ${btnBumped ? classes.bump : ''}`
+  const numberOfCartItem = items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   },0);
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnBumped(true)
+    const timer = setTimeout(() => {
+      setBtnBumped(false)
+    }, 300);
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
 
   return (
     <button className={buttonClasses} onClick={props.onClick}>
