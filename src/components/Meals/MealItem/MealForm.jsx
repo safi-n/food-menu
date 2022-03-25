c2222814784ac40e7b66306c85103af6bc03ae9a
Safi <Nurzai.safi@gmail.com>
2022-03-25 13:27:17 +0300

    Amount ref & useState are set

--- src/components/Meals/MealItem/MealForm.jsx
+++ src/components/Meals/MealItem/MealForm.jsx
@@ -1,30 +1,47 @@
-import React, {useRef} from "react";
+import React, { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./mealItem.module.css";
 
-const MealForm = props => {
+const MealForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
   const amountInputref = useRef();
 
-  const submitHandler = event => {
+  const submitHandler = (event) => {
     event.prevenDefault();
-  }
 
-  const enteredAmount = amountInputref.current.value;
-  const enterAmountNumber = +enteredAmount;
+    const enteredAmount = amountInputref.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false)
+      return;
+    } else {
+      // We need more data thats why we should pass it through props and get data from context
+      props.onAddToCart(enteredAmountNumber)
+    }
+  };
+
   return (
-    <form className={classes['input-form']} onSubmit={submitHandler}>
+    <form className={classes["input-form"]} onSubmit={submitHandler}>
       <Input
-      label='amount'
-      ref={amountInputref}
-      input={{
-        id: 'amount_' + props.id,
-        type: 'number',
-        min: '1',
-        max: "5",
-        step: '1',
-        defaultValue: '1'
-      }}/>
+        ref={amountInputref}
+        label="amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
       <button>+</button>
+      {!amountIsValid && <p>PLease enter the amount from (1-5).</p>}
     </form>
   );
 };
